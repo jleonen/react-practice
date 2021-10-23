a4a36ee268ee46de642b7c029388c46ecfe3cf56
jleonen <jleonen2012@gmail.com>
2021-10-23 00:49:54 -0700

    Added handler functions to handle input values

--- src/ClassForm.js
+++ src/ClassForm.js
@@ -2,18 +2,27 @@ import React from "react";
 import "./ClassForm.css";
 
 const ClassForm = function () {
+  const classChangeHandler = function (event) {
+    console.log(event.target.value);
+  };
+
+  const nameChangeHandler = function (event) {
+    console.log(event.target.value);
+  };
+
   return (
     <div className="container">
       <h2>Choose your class</h2>
       <label>Enter Name</label>
-      <input />
+      <input onChange={nameChangeHandler} />
       <label>Select your class</label>
-      <select>
+      <select onChange={classChangeHandler}>
         <option value="warrior">Warrior</option>
         <option value="monk">Monk</option>
         <option value="black mage">Black Mage</option>
         <option value="white mage">White Mage</option>
       </select>
+      <button type="submit">Submit class</button>
     </div>
   );
 };
